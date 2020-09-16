fa4e2637a5a86482775377caf4a8cc66f8db7d83
vagrant <vagrant@vagrant.vm>
2020-09-16 08:31:15 +0000

    0916/5pm

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ProjectsHomePageView,ProjectsInsertView,
+from .views import ProjectsHomePageView,ProjectsInsertView
 
 # from .views import ProjectsInsertPageView
 
 urlpatterns = [
     
     path('',ProjectsHomePageView.as_view(),name='projects_home'),
-    path('projects/projects_insert/',ProjectsInsertPageView.as_view(),name='projects_insert'),
+    path('projects/projects_insert/',ProjectsInsertView.as_view(),name='projects_insert'),
 ]
